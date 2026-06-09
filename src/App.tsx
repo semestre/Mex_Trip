fe9ed2d055bac929f4937696dc6602021bdb0a5c
Saristeidyn <129422890+Saristeidyn@users.noreply.github.com>
2026-06-09 15:25:54 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-500">
+        Tailwind is working 🎉
+      </h1>
+    </div>
+  )
+}
+
+export default App
